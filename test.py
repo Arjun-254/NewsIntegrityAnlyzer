00f2650bb70396c5aab81a1cba06877ae8165bc0
Arjun-254 <arjun.shah254@gmail.com>
2024-05-08 13:15:27 +0530

    Majority Voting FactCC pipeline added and working

--- test.py
+++ test.py
@@ -8,8 +8,9 @@ headers = {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
 }
 
-html = requests.get('https://www.google.com/search?q="What+is+the+age+of+donald+trump?', headers=headers)
+html = requests.get('https://www.google.com/search?q="Did Manchester United win the 2022-23 Premier League title?', headers=headers)
 soup = BeautifulSoup(html.text, 'html.parser')
 
-answer = soup.select_one('.DI6Ufb').text
-print(answer)
+answer = soup.select_one('.DI6Ufb')
+answer = answer.find(class_='Z0LcW t2b5Cf').text
+pprint(answer)
