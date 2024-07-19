50e8c459ffb7025f1967a4cdd572735d7808a5bc
Arjun-254 <arjun.shah254@gmail.com>
2024-07-19 13:33:54 +0530

    2 pipelines and generated questions for LIAR and FakeOrReal

---
+++ artifacts/scraping_selenium.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+
+def people_also_ask(url):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument('log-level=1')
+
+    cService = Service(executable_path='/Users/ameet/Desktop/IPD-FastAPI/chromedriver')
+    driver = webdriver.Chrome(service=cService, options=chrome_options)
+    driver.get(url)
+    time.sleep(3)  
+    
+    questions = driver.find_elements(By.XPATH, "//div[@jsname='yEVEwb']")
+    
+    all_span_texts = []  
+    
+    for div in questions:
+        try:
+            div.click()
+            time.sleep(1)
+            
+            span_elements = div.find_elements(By.TAG_NAME, 'span')
+            for span in span_elements:
+                text = span.text.strip()
+                if text and text not in all_span_texts:
+                    all_span_texts.append(text)
+                    
+        except Exception as e:
+            print(f"Error clicking or extracting from div/span: {e}")
+    
+    driver.quit()
+    return all_span_texts[0:2]
+
